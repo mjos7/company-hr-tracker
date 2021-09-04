b7ea38f61a433257448c889fcb1eecfdf3cecfa0
mjos7 <83667780+mjos7@users.noreply.github.com>
2021-09-03 18:50:45 -0700

    schema.sql for roles, employees and departments

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
+);
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employees (
+   id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_name VARCHAR(30) DEFAULT 'NONE',
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
+);
